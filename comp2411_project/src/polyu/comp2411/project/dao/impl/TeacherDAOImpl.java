56f8c3f8dced62ee4bf527f9ec39dc29bf742030
Yookivivi <74478753+Yookivivi@users.noreply.github.com>
2020-11-30 22:50:58 +0800

    Update TeacherDAOImpl.java

--- comp2411_project/src/polyu/comp2411/project/dao/impl/TeacherDAOImpl.java
+++ comp2411_project/src/polyu/comp2411/project/dao/impl/TeacherDAOImpl.java
@@ -1,29 +1,122 @@
-package polyu.comp2411.project.dao.impl;
+ackage polyu.comp2411.project.dao.impl;
 
 import polyu.comp2411.project.dao.TeacherDAO;
 import polyu.comp2411.project.entity.Classe;
 import polyu.comp2411.project.entity.Teacher;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TeacherDAOImpl extends BaseDAO  implements TeacherDAO {
+    private Teacher teacher;
+    private ResultSet rs;
+
+    private ResultSet result;
+    TeacherDAOImpl(){
+        super();
+    }
+
+
     @Override
     public Teacher serchByID(int id) {
-        return null;
+        String sql = "SELECT * FROM TEACHER WHERE TEACHER_ID = ?";
+        try{
+            setPs(sql);
+            getPs().setInt(1,id);
+            rs = getPs().executeQuery();
+            while(rs.next()){
+
+                String name = rs.getString("TEACHER_NAME");
+                Teacher result = new Teacher(id,name);
+                this.teacher= result; //set the teacher field as this.
+                return result;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            closeConn();
+            closeStatement();
+            closePreparedStatement();
+        }return null;
     }
 
     @Override
     public void addTeacher(Teacher tc) {
-
+        String sql = "INSERT INTO TEACHER VALUES(?,?)"; // parameter to be set later
+        try {
+            setPs(sql);
+            //set parameter of sql
+            getPs().setInt(1,tc.getId());
+            getPs().setString(2,tc.getName());
+            getPs().execute();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            // close resources used
+            closeConn();
+            closeStatement();
+            closePreparedStatement();
+        }
     }
 
     @Override
     public List<Teacher> searchByClass(Classe cls) {
+        String sql = "SELECT CLASS_NO,DISTINCT TEACHER_ID,TEACHER_NAME " +
+                "FROM TEACHER,TEACH_IN " +
+                "WHERE CLASS_NO = ? and TEACHER.TEACHER_ID=TEACH_IN.TEACHER_ID";
+        try{
+            setPs(sql);
+            getPs().setInt(1,cls.getClassNo());
+            rs = getPs().executeQuery(sql);
+            List<Teacher> ans = new ArrayList<>();
+            while(rs.next()){
+                int id = rs.getInt("TEACHER_ID");
+                String name = rs.getString("TEACHER_NAME");
+                ans.add(new Teacher(id,name));
+            }
+            return ans;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            // close resources used
+            closeConn();
+            closeStatement();
+            closePreparedStatement();
+        }
         return null;
     }
 
     @Override
     public Teacher searchByClassTeacherOf(Classe cls) {
+
+        String sql = "SELECT CLASS_NO,DISTINCT CLASS_TEACHER_ID,TEACHER_NAME " +
+                "FROM TEACHER,CLASS" +
+                "WHERE CLASS_NO = ? and TEACHER_ID=CLASS_TEACHER_ID";
+        try{
+            setPs(sql);
+            getPs().setInt(1,cls.getClassNo());
+            rs = getPs().executeQuery(sql);
+            while(rs.next()){
+                int id = rs.getInt("TEACHER_ID");
+                String name = rs.getString("TEACHER_NAME");
+                Teacher ans=new Teacher(id,name);
+                this.teacher=ans;
+                return ans;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            // close resources used
+            closeConn();
+            closeStatement();
+            closePreparedStatement();
+        }
         return null;
     }
 }
