6071a7aeac4f47f35e6539613739fe7609604d58
Yookivivi <74478753+Yookivivi@users.noreply.github.com>
2020-12-05 20:33:49 +0800

    Update ScoreList.java

--- comp2411_project/src/polyu/comp2411/project/controller/ScoreList.java
+++ comp2411_project/src/polyu/comp2411/project/controller/ScoreList.java
@@ -1,7 +1,16 @@
 package polyu.comp2411.project.controller;
 
+import polyu.comp2411.project.entity.*;
+import polyu.comp2411.project.dao.impl.ScoreListDAOImpl;
+import polyu.comp2411.project.dao.ScoreListDAO;
+
+
 /**
  * students use this to view his score(letter grade) of all the exams
  */
-public class ScoreList {
+public class ShowScoreList {
+    public void showScoreList(Student student){
+        ScoreListDAO scoreList=new ScoreListDAOImpl();
+        System.out.println(scoreList.searchByStudent(student));
+    }
 }
