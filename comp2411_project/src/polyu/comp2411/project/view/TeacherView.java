e4fcd336641655f3e7c2227673ede8f7a3033aba
Yookivivi <74478753+Yookivivi@users.noreply.github.com>
2020-12-06 00:45:32 +0800

    Update TeacherView.java

--- comp2411_project/src/polyu/comp2411/project/view/TeacherView.java
+++ comp2411_project/src/polyu/comp2411/project/view/TeacherView.java
@@ -73,4 +73,8 @@ public class TeacherView {
             System.out.println("Error: "+e+"please contact admin!");
         }
     }
+    public static void main(String[] args) {
+        TeacherView teacherView = new TeacherView();
+        teacherView.teacherView();
+    }
 }
