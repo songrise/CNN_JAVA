c01b47da45805c398604b751e2fcafdbebf1ff34
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 02:18:33 +0800

    Update ManualJudgeService.java

--- comp2411_project/src/polyu/comp2411/project/service/ManualJudgeService.java
+++ comp2411_project/src/polyu/comp2411/project/service/ManualJudgeService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface ManualJudgeService {
     /**
      * fetch all full-length question of an exam for manual judge.
+     *
      * @param ex the exam
      * @return a list of full length quesion in this exam.
      */
