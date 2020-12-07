b994d33b06aec8b54369e12e87878d2991173297
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 01:51:33 +0800

    Update ExamDAO.java

--- comp2411_project/src/polyu/comp2411/project/dao/ExamDAO.java
+++ comp2411_project/src/polyu/comp2411/project/dao/ExamDAO.java
@@ -9,11 +9,17 @@ import java.util.List;
 public interface ExamDAO {
 
     Exam searchByID(int id);
+
     void addExam(Exam ex);
+
     void delExam(Exam ex);
+
     void updExam(Exam oldEx, Exam newEx);
+
     List<Exam> searchBySubject(Subject sub);
+
     List<Exam> searchBySubAndClass(Subject sub, Classe cls);
+
     //get the next question number of an exam
     int getNextExamId();
 }
