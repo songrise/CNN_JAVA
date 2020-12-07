b7f4239d7b8b3dd2bc68aea9f2b5d8358d97214f
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-07 20:47:16 +0800

    Update AccountManager.java

--- comp2411_project/src/polyu/comp2411/project/controller/AccountManager.java
+++ comp2411_project/src/polyu/comp2411/project/controller/AccountManager.java
@@ -140,17 +140,9 @@ public class AccountManager {
             System.out.println("two inputs not match!");
         }
 
-        while (true) {
-            System.out.print("Are you sure to reset the password as " + pswd + "(y/n): ");
-            String YorN = sc.nextLine();
-
-            if (YorN.toUpperCase().equals("Y"))
-                accountService.changePassword(uid, pswd);
-            else if (!YorN.toUpperCase().equals("N")) {
-                continue;
-            }
-            break;
-        }
+        System.out.print("Are you sure to reset the password as " + pswd + "(y/n): ");
+        if (sc.nextLine().toUpperCase().equals("Y"))
+            accountService.changePassword(uid, pswd);
     }
 
     public static void main(String[] args) {
