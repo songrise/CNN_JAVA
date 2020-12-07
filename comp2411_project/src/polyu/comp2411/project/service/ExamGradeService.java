8608e1e098abb0f2b33de3da54f41f8097c4df18
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 02:16:21 +0800

    Update ExamGradeService.java

--- comp2411_project/src/polyu/comp2411/project/service/ExamGradeService.java
+++ comp2411_project/src/polyu/comp2411/project/service/ExamGradeService.java
@@ -6,14 +6,16 @@ public interface ExamGradeService {
      * all the student participated in this exam will be
      * calculated. The score means original points, not
      * letter grade.
-     *
+     * <p>
      * The score a student get should be porpotional to the
      * full mark. (i.e. normalized to x/100 points)
-     *
+     * <p>
      * When this method are called, all question shall already
      * be judged.
+     *
      * @param testId
      */
     void calScoreOfExam(int testId);
+
     void addFeedback(int testId, int stuId, String commnet);
 }
