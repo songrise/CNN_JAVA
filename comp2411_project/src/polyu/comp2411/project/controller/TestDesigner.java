8dbf708ffa07506b6960edf6538fb4fa7cafe1dc
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 00:21:40 +0800

    Update TestDesigner.java

--- comp2411_project/src/polyu/comp2411/project/controller/TestDesigner.java
+++ comp2411_project/src/polyu/comp2411/project/controller/TestDesigner.java
@@ -7,92 +7,148 @@ import polyu.comp2411.project.service.impl.TestDesignerServiceImpl;
 
 import java.util.Scanner;
 
-
 /**
  * teacher use this to design a test.
  */
 public class TestDesigner {
-  public void createTest(int arrangerId){
-    Scanner sc=new Scanner(System.in);
-    TestDesignerService testDesignerService = new TestDesignerServiceImpl();
-    int clsId, subId,duration;
-    String startTime;
+    public void createTest(int arrangerId) {
+        Scanner sc = new Scanner(System.in);
+        TestDesignerService testDesignerService = new TestDesignerServiceImpl();
+        int clsId, subId, duration;
+        String startTime;
 
+        while (true) {
+            System.out.print("Please input the class id: ");
+            try {
+                String input = sc.nextLine().trim();
+                if (input.length() == 6) {
+                    clsId = Integer.parseInt(input);
+                    break;
+                }
+            } catch (NumberFormatException ignored) {
+            }
+            System.out.println("The id should be 6 digits!");
+        }
 
-    System.out.printf("Please input the class id: ");
-    clsId = sc.nextInt();
-    sc.nextLine();
+        while (true) {
+            System.out.print("Please input the subject id: ");
+            try {
+                String input = sc.nextLine().trim();
+                if (input.length() == 6) {
+                    subId = Integer.parseInt(input);
+                    break;
+                }
+            } catch (NumberFormatException ignored) {
+            }
+            System.out.println("The id should be 6 digits!");
+        }
 
-    System.out.printf("Please input the subject id: ");
-    subId = sc.nextInt();
-    sc.nextLine();
+        while (true) {
+            System.out.print("Please input the test duration in minutes: ");
+            try {
+                duration = Integer.parseInt(sc.nextLine().trim());
+                break;
+            } catch (NumberFormatException ignored) {
+            }
+            System.out.println("The duration should be an integer!");
+        }
 
-    System.out.printf("Please input the test duration in minutes: ");
-    duration = sc.nextInt();
-    sc.nextLine();
+        System.out.printf("Please input the test start time format:yyyy-[m]m-[d]d hh:mm:ss[.f...] : ");
+        startTime = sc.nextLine();
 
-    System.out.printf("Please input the test start time format:yyyy-[m]m-[d]d hh:mm:ss[.f...] : ");
-    startTime = sc.nextLine();
-    int examId = testDesignerService.createTest(arrangerId,clsId,subId,duration,startTime);
-    if (examId == -1){
-      throw new ServiceException("You cannot create this exam!");
+        int examId = testDesignerService.createTest(arrangerId, clsId, subId, duration, startTime);
+        if (examId == -1) {
+            throw new ServiceException("You cannot create this exam!");
+        }
+        System.out.printf("Successfully created test with id = " + examId);
     }
-    System.out.printf("Successfully created test with id = "+examId);
-  }
-  
-  
-  
-  
-  public void createQuestions(){
-    Scanner sc=new Scanner(System.in);
-    TestDesignerService testDesignerService = new TestDesignerServiceImpl();
-    int qCount = 0;
-    System.out.printf("\nPlease input the ID of exam: ");
-    int examId = sc.nextInt();
-    sc.nextLine();
 
-    while (true){
-      String description,type,answer;
-      boolean isCompulsory = false;
-      int score;
+    public void createQuestions() {
+        Scanner sc = new Scanner(System.in);
+        TestDesignerService testDesignerService = new TestDesignerServiceImpl();
+        int qCount = 0;
 
-      System.out.printf("Please input the description of the question: ");
+        int examId;
+        while (true) {
+            System.out.print("\nPlease input the ID of exam: ");
+            try {
+                String input = sc.nextLine().trim();
+                if (input.length() == 6) {
+                    examId = Integer.parseInt(input);
+                    break;
+                }
+            } catch (NumberFormatException ignored) {
+            }
+            System.out.println("The id should be 6 digits!");
+        }
+
+        while (true) {
+            String description, type, answer;
+            boolean isCompulsory = false;
+            int score;
+
+            System.out.printf("Please input the description of the question: ");
 //      if (sc.hasNextLine())
-        description = sc.nextLine();
+            description = sc.nextLine();
 
-      System.out.printf("Please input whether this question is compulsory(yes/no): ");
-      String s = sc.nextLine();
-      if(s.toUpperCase().equals("YES"))
-        isCompulsory = true;
+            while (true) {
+                System.out.printf("Please input whether this question is compulsory(yes/no): ");
+                String input = sc.nextLine().trim().toUpperCase();
+                if (input.equals("YES"))
+                    isCompulsory = true;
+                else if (!input.equals("NO"))
+                    continue;
+                break;
+            }
 
+            while (true) {
+                System.out.printf("Please input this question's type, it is multiple choice, \nfill in the blank, or standard full-length test question (MC/FB/FL): ");
+                String input = sc.nextLine().trim().toUpperCase();
+                if (input.equals("MC") || input.equals("FB") || input.equals("FL")) {
+                    type = input;
+                    break;
+                }
+            }
 
+            System.out.printf("Please input the answer of the question: ");
+            answer = sc.nextLine();
 
-      System.out.printf("Please input this question's type, it is multiple choice, \nfill in the blank, or standard full-length test question (MC/FB/FL): ");
-      type=sc.nextLine().toUpperCase();
+            while (true) {
+                System.out.print("Please input the score of the question: ");
+                try {
+                    score = Integer.parseInt(sc.nextLine().trim());
+                    break;
+                } catch (NumberFormatException ignored) {
+                }
+                System.out.println("The score should be an integer!");
+            }
 
-      System.out.printf("Please input the answer of the question: ");
+            testDesignerService.createQuestion(examId, description, isCompulsory, type, answer, score);
+            qCount += 1;
 
-      answer=sc.nextLine();
+            boolean isContinue = false;
+            while (true) {
+                System.out.print("You have added " + qCount + " questions, continue? (Y/N): ");
+                String input = sc.nextLine().trim().toUpperCase();
 
-      System.out.printf("Please input the score of the question: ");
-      score = sc.nextInt();
-      sc.nextLine();
-      testDesignerService.createQuestion(examId,description,isCompulsory,type,answer,score);
-      qCount+=1;
-      System.out.print("You have added "+qCount+ " questions, continue? (y/N): ");
-      String ifContinue = sc.nextLine();
-      if (ifContinue.toUpperCase().equals("N")){
-        System.out.printf("Exited.\n");
-        break;
-      }
+                if (input.equals("Y"))
+                    isContinue = true;
+                else if (!input.equals("N"))
+                    continue;
+                break;
+            }
+
+            if (!isContinue) {
+                System.out.printf("Exited.\n");
+                break;
+            }
+        }
     }
-  }
 
-
-  public static void main(String[] args) {
-    Teacher test = new Teacher(000001,"Test");
-    TestDesigner testDesigner = new TestDesigner();
+    public static void main(String[] args) {
+        Teacher test = new Teacher(000001, "Test");
+        TestDesigner testDesigner = new TestDesigner();
 //  testDesigner.createTest(test);
-    testDesigner.createQuestions();
-  }
+        testDesigner.createQuestions();
+    }
 }
