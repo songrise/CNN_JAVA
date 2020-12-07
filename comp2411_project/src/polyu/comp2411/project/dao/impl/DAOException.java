f4d16042e477fbe6fa58eb03e4524c7c7e5d91ca
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-07 22:28:25 +0800

    Update DAOException.java

--- comp2411_project/src/polyu/comp2411/project/dao/impl/DAOException.java
+++ comp2411_project/src/polyu/comp2411/project/dao/impl/DAOException.java
@@ -1,10 +1,11 @@
 package polyu.comp2411.project.dao.impl;
 
-public class DAOException extends RuntimeException{
-    public DAOException(){
+public class DAOException extends RuntimeException {
+    public DAOException() {
         super();
     }
-    public DAOException(String msg){
+
+    public DAOException(String msg) {
         super(msg);
     }
 }
