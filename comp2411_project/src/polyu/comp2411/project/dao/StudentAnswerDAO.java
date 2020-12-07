e284c1ea807b2fc736437a606e0429ef149da60b
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 01:55:27 +0800

    Update StudentAnswerDAO.java

--- comp2411_project/src/polyu/comp2411/project/dao/StudentAnswerDAO.java
+++ comp2411_project/src/polyu/comp2411/project/dao/StudentAnswerDAO.java
@@ -10,11 +10,17 @@ import java.util.List;
 public interface StudentAnswerDAO {
     //search by studentId examId, questionNo, which together is primary key of studentAnswer
     StudentAnswer searchByKey(Student stu, Exam ex, Question que);
+
     void addStudentAnswer(StudentAnswer stuAns);
+
     void delStudentAnswer(StudentAnswer stuAns);
+
     void updStudentAnswer(StudentAnswer oldStuAns, StudentAnswer newStudentAns);
+
     List<StudentAnswer> searchByStudent(Student stu);
+
     List<StudentAnswer> searchByExam(Exam ex);
+
     // primary key of quesion is testID + quesion No.
     List<StudentAnswer> searchByQuestion(Question que);
 
