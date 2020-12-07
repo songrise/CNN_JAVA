8698a9230c60db2b70976fc05fd489a7c2c8588f
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-07 20:13:17 +0800

    Update Main.java

--- comp2411_project/src/polyu/comp2411/project/Main.java
+++ comp2411_project/src/polyu/comp2411/project/Main.java
@@ -6,17 +6,16 @@ import polyu.comp2411.project.view.StudentView;
 import polyu.comp2411.project.view.TeacherView;
 
 public class Main {
-
     public static void main(String[] args) {
         LoginView loginView = new LoginView();
         loginView.view();
         int uid = loginView.getUid();
-        int priviledge = loginView.getPriviledge();
-        switch (priviledge){
+        int privilege = loginView.getPriviledge();
+
+        switch (privilege){
             case 0:
                 AdminView adminView = new AdminView(uid);
                 adminView.view();
-
                 break;
             case 1:
                 TeacherView teacherView = new TeacherView(uid);
@@ -26,7 +25,6 @@ public class Main {
                 StudentView studentView = new StudentView(uid);
                 studentView.stuView();
                 break;
-
         }
     }
 }
