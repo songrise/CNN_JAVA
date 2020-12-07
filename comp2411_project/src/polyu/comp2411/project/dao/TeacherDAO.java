ada8fbe455fa1bc995b01b0d29619a9291bec118
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 01:57:40 +0800

    Update TeacherDAO.java

--- comp2411_project/src/polyu/comp2411/project/dao/TeacherDAO.java
+++ comp2411_project/src/polyu/comp2411/project/dao/TeacherDAO.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 public interface TeacherDAO {
     Teacher serchByID(int id);
+
     void addTeacher(Teacher tc);
 
     //this is search subject teacher
     List<Teacher> searchByClass(Classe cls);
+
     // this is search class teacher
     Teacher searchByClassTeacherOf(Classe cls);
 }
