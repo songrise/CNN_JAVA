7631d2774b8d754d93b74261541166f6915bee70
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 02:20:06 +0800

    Update PerformanceAnalysisService.java

--- comp2411_project/src/polyu/comp2411/project/service/PerformanceAnalysisService.java
+++ comp2411_project/src/polyu/comp2411/project/service/PerformanceAnalysisService.java
@@ -12,21 +12,24 @@ public interface PerformanceAnalysisService {
 
     /**
      * calculate for all subjects offered in this class the averange score of it
+     *
      * @param cls
      * @return a map contained subjects and its corresponding avg. e.g.: <"Math", 85.4>
      */
-    Map<String,Double> subjectAvgs(Classe cls);
+    Map<String, Double> subjectAvgs(Classe cls);
 
     /**
      * calculate for all subjects offered in this class the variance of score.
+     *
      * @param cls
      * @return a map contained subjects and its corresponding var. e.g.: <"Math", 14.9>
      */
-    Map<String,Double> subjectVars(Classe cls);
+    Map<String, Double> subjectVars(Classe cls);
 
     /**
      * export an anlysis report. todo
+     *
      * @param analysis
      */
-    void exportAnalysis(Map<String,Double> analysis);
+    void exportAnalysis(Map<String, Double> analysis);
 }
