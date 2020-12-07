0510a8e91fc06d4bacf1eaa4650c2ad19a99ae4f
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 01:31:26 +0800

    Update ScoreListDisplay.java

--- comp2411_project/src/polyu/comp2411/project/controller/ScoreListDisplay.java
+++ comp2411_project/src/polyu/comp2411/project/controller/ScoreListDisplay.java
@@ -8,65 +8,57 @@ import polyu.comp2411.project.service.ScoreListService;
 import polyu.comp2411.project.service.impl.ScoreListServiceImpl;
 
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * students use this to view his score(letter grade) of all the exams
  */
 public class ScoreListDisplay {
-    public static void showScoreList(int stuid){
-        Scanner sc=new Scanner(System.in);
-        StudentDAO student=new StudentDAOImpl();
+    public static void showScoreList(int stuid) {
+        StudentDAO student = new StudentDAOImpl();
         Student stu = student.searchByID(stuid);
-        System.out.println("Student "+stu.getName()+", here is your transcript: ");
+        System.out.println("Student " + stu.getName() + ", here is your transcript: ");
         ScoreListService scoreListService = new ScoreListServiceImpl();
         List<ScoreList> sl = scoreListService.getScoreList(stuid);
-        if (sl.isEmpty()){
+        if (sl.isEmpty()) {
             System.out.printf("Seems you have no exam record\n\n");
-        }
-        else {
 
+        } else {
             System.out.printf("Test ID\t\tGrade\t\tFeedback\n");
-            for (ScoreList record: sl){
-                System.out.printf("%06d\t\t%s\t\t%s\n",record.getTestId(),convertToLetterGrade(record.getScore()),record.getFeedBack());
+            for (ScoreList record : sl) {
+                System.out.printf("%06d\t\t%s\t\t%s\n", record.getTestId(), convertToLetterGrade(record.getScore()), record.getFeedBack());
             }
         }
     }
 
-    private static String convertToLetterGrade(int grade){
-        assert grade>=0 && grade<=100;
-        if (grade<40){
+    private static String convertToLetterGrade(int grade) {
+        assert grade >= 0 && grade <= 100;
+        if (grade < 40) {
             return "F";
-        }
-        else if(grade<45){
+        } else if (grade < 45) {
             return "D-";
-        }else if(grade<50){
+        } else if (grade < 50) {
             return "D";
-        }
-        else if(grade<55){
+        } else if (grade < 55) {
             return "D+";
-        }else if(grade < 60){
+        } else if (grade < 60) {
             return "C-";
-        }else if(grade < 65){
+        } else if (grade < 65) {
             return "C";
-        }else if(grade < 70){
+        } else if (grade < 70) {
             return "C+";
-        }else if(grade < 75){
+        } else if (grade < 75) {
             return "B-";
-        }else if(grade < 80){
+        } else if (grade < 80) {
             return "B";
-        }else if(grade < 85){
+        } else if (grade < 85) {
             return "B+";
-        }else if(grade < 90){
+        } else if (grade < 90) {
             return "A-";
-        }else if(grade < 95){
+        } else if (grade < 95) {
             return "A";
-        }else if(grade < 100){
+        } else if (grade < 100) {
             return "A+";
         }
         return "F";
-
     }
-
 }
-
