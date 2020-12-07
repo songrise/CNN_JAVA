caad10e1c56ec095affd5b6b6ac240ace322b3bf
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 02:15:43 +0800

    Update AutoJudgeService.java

--- comp2411_project/src/polyu/comp2411/project/service/AutoJudgeService.java
+++ comp2411_project/src/polyu/comp2411/project/service/AutoJudgeService.java
@@ -4,6 +4,7 @@ public interface AutoJudgeService {
     /**
      * auto judge a particular exam, all student participated in this exam
      * will be judged.
+     *
      * @param testId the exam
      */
     void judgeAnExam(int testId);
