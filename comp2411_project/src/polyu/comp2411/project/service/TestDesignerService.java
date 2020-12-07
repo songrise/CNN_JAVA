3f3f3e8cc63ad2754fdf85026aebaf6170913e05
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 02:20:56 +0800

    Update TestDesignerService.java

--- comp2411_project/src/polyu/comp2411/project/service/TestDesignerService.java
+++ comp2411_project/src/polyu/comp2411/project/service/TestDesignerService.java
@@ -5,6 +5,7 @@ public interface TestDesignerService {
      * create test has following to steps: create a new Test entity and write it to DB.
      * for all student in that class, add this exam to their exam list. This method only
      * create a empty exam with no questions.
+     *
      * @param arrangerID
      * @param clsID
      * @param subID
@@ -13,12 +14,14 @@ public interface TestDesignerService {
      * @return id of the test
      */
     int createTest(int arrangerID, int clsID, int subID, int testDuration, String startTime);
+
     /**
      * create a question and add it to an exam.
+     *
      * @param ex
-     * @param qDesc: description of question
+     * @param qDesc:  description of question
      * @param answer: correct answer
-     * @param type: type of exam, should be 'MC' or 'FL' or 'FB'
+     * @param type:   type of exam, should be 'MC' or 'FL' or 'FB'
      * @return the No. of newly added question
      */
     int createQuestion(int examID, String qDesc, boolean isCompulsory, String type, String answer, int score);
