3158afacd2f22bc4396b07f037cdd339075addee
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2020-12-08 00:40:40 +0800

    Update ExamListDisplay.java

--- comp2411_project/src/polyu/comp2411/project/controller/ExamListDisplay.java
+++ comp2411_project/src/polyu/comp2411/project/controller/ExamListDisplay.java
@@ -6,19 +6,16 @@ import polyu.comp2411.project.service.impl.ExamListServiceImpl;
 import java.util.Map;
 
 public class ExamListDisplay {
-
-        public static void showExamList(int stuid) {
-
-            ExamListService examListService = new ExamListServiceImpl();
-            Map<Integer, String> schedule = examListService.getUpcomingTest(stuid);
-            if (schedule.isEmpty()) {
-                System.out.println("Seems you have no upcoming exam\n\n");
-            } else {
-                System.out.print("Test ID\t\t\tStart Time\n");
-                for (Integer recordID : schedule.keySet()) {
-                    System.out.printf("%06d\t\t\t\t%s\n", recordID, schedule.get(recordID));
-                }
+    public static void showExamList(int stuid) {
+        ExamListService examListService = new ExamListServiceImpl();
+        Map<Integer, String> schedule = examListService.getUpcomingTest(stuid);
+        if (schedule.isEmpty()) {
+            System.out.println("Seems you have no upcoming exam\n\n");
+        } else {
+            System.out.print("Test ID\t\t\tStart Time\n");
+            for (Integer recordID : schedule.keySet()) {
+                System.out.printf("%06d\t\t\t\t%s\n", recordID, schedule.get(recordID));
             }
         }
-
+    }
 }
